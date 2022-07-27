0fe92c36d7558157d0558495442f1519116cbe41
itay <itayo@inuitive-tech.com>
2022-07-27 18:24:12 +0300

    1. evaluation code added 2. file_manager.py module added which connects the app to dedicated gdrive. 3. basic functionality added to main

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-# The main flow should be here
+import file_manager
+
+if __name__ == '__main__':
+    # load files
+    drive = file_manager.DriveAPI()
--- main_streamlit_page.py
+++ main_streamlit_page.py
@@ -5,7 +5,7 @@ st.markdown(
     """
 <style>
 .css-1v3fvcr {
-    background-color: ghostwhite !important;
+    background-color: lightgray !important;
     height: 100vh;
     width: 100vw;
     }
@@ -21,28 +21,28 @@ st.markdown(nlp, unsafe_allow_html=True)
 title = '<p style="font-family:Courier; text-align:center; color:Blue; font-size: 40px;">Ukraine-Russia conflict tweet text generator</p>'
 st.markdown(title, unsafe_allow_html=True)
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+# DATE_COLUMN = 'date/time'
+# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+#
+# @st.cache
+# def load_data(nrows):
+#     data = pd.read_csv(DATA_URL, nrows=nrows)
+#     lowercase = lambda x: str(x).lower()
+#     data.rename(lowercase, axis='columns', inplace=True)
+#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+#     return data
+#
+# data_load_state = st.text('Loading data...')
+# data = load_data(10000)
+# data_load_state.text("Done! (using st.cache)")
 
 text_placeholder = st.empty()
 header = '<p style="color:darkred; font-size: 15px;">*Fill all the fields before run:</p>'
 text_placeholder.markdown(header, unsafe_allow_html=True)
 
 radio_placeholder = st.empty()
-text_input_placeholder = st.empty();
+text_input_placeholder = st.empty()
 
 option = radio_placeholder.radio(
      "Please choose generation or autocomplete",
@@ -55,17 +55,16 @@ placeholder = st.empty()
 form = placeholder.form("my_form")
 form.selectbox('Please select model', ['model1', 'model2', 'model3', 'model4', 'model5'])
 form.text('Please inferred amount of characters')
-number = '<input type="number" min="0" max="100" placeholder="" ' \
-         'style="color:green; width: 42vw; height: 7vh;"></input>'
+number = '<input type="number" min="0" max="100" placeholder="" style="color:green;"></input>'
 form.markdown(number, unsafe_allow_html=True)
-form.select_slider('Please select how many words to generate in the tweet',
-                   ['top10k', 'top50k', 'top100k', 'top1m', 'top1ksampled', 'Random :)'])
+#form.select_slider('Please select how many words to generate in the tweet',
+#                   ['top10k', 'top50k', 'top100k', 'top1m', 'top1ksampled', 'Random :)'])
 
 if form.form_submit_button('Generate Some Text'):
-    placeholder.empty()
-    text_placeholder.empty()
-    radio_placeholder.empty()
-    text_input_placeholder.empty()
+    # placeholder.empty()
+    # text_placeholder.empty()
+    # radio_placeholder.empty()
+    # text_input_placeholder.empty()
     text = "'tweet data: bla bla...................................'"
     st.write(text)
 
